c53e73c633d4c253783b95c918021b41c62f6550
Kirk Roerig <kirk.roerig@modustri.com>
2016-10-23 10:15:43 -0400

    Missed some files

--- openai/agent.py
+++ openai/agent.py
@@ -3,67 +3,125 @@ from collections import namedtuple
 from random import randint
 import random
 
-# Obs[ 1 * 8 ] * M[8 * 4] -> Act[4, 1]
- #
- # A[ M, 1 ] * B[ N, M ] = [
- #                    [],
- # 					  [],
- # 					  [],
- # 					  []
- # 				      ]
+TRAINING_EPOCHS = 200
+GENERATION_SIZE = 500
+RANDOMIZATION = 2
 
-def randcol(): return randint(0, 7)
-def randrow(): return randint(0, 3)
 
 DESC_SHAPE = (8, 4)
 
-class Transition:
-	def __init__(self,
-	             pre_action_obs = None,
-	             action = None,
-	             reward = None,
-	             done   = None,
-	             post_action_obs = None):
-		self.pre_action_observation = pre_action_observation
-		self.post_action_observation = post_action_obs
-		self.action = action
-		self.reward = reward
-		self.done   = done
+def rnd_white(base=None, mag=1):
+	matrix = (np.random.random(DESC_SHAPE) * 2 - 1) * mag
 
+	if base is not None:
+		matrix = matrix + base
+
+	return matrix
+
+def rnd_gaussian(base=None, mag=1, std_dev=0.25):
+	matrix = (np.random.normal(0, std_dev, DESC_SHAPE) * 2 - 1) * mag
+
+	if base is not None:
+		matrix = matrix + base
+
+	return matrix
 
 class Agent:
-	def __init__(self):
-		self.last_reward = np.zeros(DESC_SHAPE)
-		self.past_transitions = []
-		self.params = np.random.random(DESC_SHAPE)
+	def __init__(self, env, maximize=True):
+		self.environment = env
+		self.best_candidate = None
+		self.candidates = []
+		self.maximize = maximize
+		self.gen_candidates(std_dev=1)
+		self.improvement_rate = 0
 
-		self.delta_params = np.random.random(DESC_SHAPE)
-		self.first = True
+		try:
+			self.best_candidate = best_candidate = np.loadtxt(fname='params.txt', delimiter = ",")
+			self.candidates += [best_candidate]
+			print("Loaded existing params")
+		except:
+			pass
 
+	def gen_candidates(self, base=None, std_dev=0.25):
+		self.candidates = []
 
-	def train(self, observation, reward, improved):
-		self.delta_params = (np.random.random(DESC_SHAPE) * 2 - 1) * 0.1
-		self.params += self.delta_params
-		# self.params /= self.params.max()
+		if isinstance(base, np.ndarray):
+			self.candidates += [base]
+			for _ in range(GENERATION_SIZE):
+				self.candidates += [rnd_gaussian(base=base, mag=RANDOMIZATION, std_dev=std_dev)]
+		elif isinstance(base, list) and isinstance(base[0], np.ndarray):
+			self.candidates += base
 
-	def step(self, observation, reward, done):
-		observation = np.array(observation)
-		action_probs = observation.dot(self.params)
-		action = np.argmax(action_probs)
+			for i in range(GENERATION_SIZE):
+				self.candidates += [rnd_gaussian(base=base[i % len(base)], mag=RANDOMIZATION, std_dev=std_dev)]
+		else:
+			for _ in range(GENERATION_SIZE):
+				self.candidates += [rnd_gaussian(mag=RANDOMIZATION, std_dev=std_dev)]
 
-		this_epoch = None
+	def run_epoch(self, desc_matrix=None, show=False):
+		total_reward = 0
 
-		if len(self.past_transitions):
-			last_transition = self.past_transitions[-1]
-			last_transition.post_action_obs = observation.copy()
-			last_transition.reward = reward
-			last_transition.done = done
+		if desc_matrix is None:
+			desc_matrix = self.best_candidate
 
-		this_epoch = Transition(
-			pre_action_obs = observation.copy(),
-			action = action
-		)
+		self.environment.reset()
+		observation, reward, done, info = self.environment.step(0)			
 
-		self.past_transitions.append(this_epoch)
+		for _ in range(600):
+			observation = np.array(observation)
+			action_probs = observation.dot(desc_matrix)
+			
+			if show:
+				self.environment.render()
+			observation, reward, done, info = self.environment.step(np.argmax(action_probs))
+			total_reward += reward
 
-		return action
+			if done:
+				break
+
+		return total_reward
+
+	def evaluate_generation(self):
+		candidate_score = []
+		avg_score = 0
+
+		# score the existing candidates
+		for candidate in self.candidates:
+			score = self.run_epoch(desc_matrix=candidate)
+			avg_score += score / float(len(self.candidates))
+
+			candidate_score += [(candidate, score)]
+
+		candidate_score = sorted(candidate_score, key=lambda cs_tuple: cs_tuple[1])
+
+		best_candidates = []
+		for _ in range(5):
+			candidate = candidate_score.pop()
+			best_candidates += [(candidate[0].copy(), candidate[1])]
+
+		return best_candidates, avg_score
+
+	def solve(self):
+		last_gen_avg = 0
+
+		for i in range(TRAINING_EPOCHS):
+			best_candidates, avg_score = self.evaluate_generation()
+
+			if i is 0:
+				last_gen_avg = avg_score
+
+			self.improvement_rate = (avg_score - last_gen_avg)
+			last_gen_avg = avg_score
+
+			best_best = best_candidates[len(best_candidates) - 1]
+			print("(%d/%d) Best score: %f Avg: %f Improvement: %f" % (i, TRAINING_EPOCHS, best_best[1], avg_score, self.improvement_rate))
+
+			if avg_score >= 200:
+				break
+			best_matrices = []
+			for candidate in best_candidates: best_matrices += [candidate[0]]
+
+			self.gen_candidates(base=best_matrices, std_dev = 10. / max(self.improvement_rate, 1))
+			self.best_candidate = best_matrices.pop()
+	
+			np.savetxt(fname='params.txt', X=self.best_candidate, delimiter=',')
--- openai/lander.py
+++ openai/lander.py
@@ -1,41 +1,13 @@
 import gym
-from agent_0 import Agent
-import copy
+from agent import Agent
+from getopt import getopt
+import sys
 
 env = gym.make('LunarLander-v2')
-observation = env.reset()
-agent = Agent()
+agent = Agent(env)
 
-action=0
-best=None
-best_agent=None
+if not '--just-show' in sys.argv:
+	print("Solving")
+	agent.solve()
 
-epochs = 2000
-for e in range(0, epochs):
-	observation = env.reset()
-	total_reward = 0
-
-	observation, reward, done, info = env.step(0)
-	agent.step(observation, reward, false)
-
-	for t in range(0, 1000):
-
-		observation, reward, done, info = env.step(action)
-
-		action = agent.step(observation, reward)
-		total_reward += reward
-
-		if done:
-			# print("{}/{} Episode finished after {} timesteps with {}".format(e, epochs, t+1, total_reward))
-			break
-
-observation = env.reset()
-total_reward = 0
-for t in range(0, 1000):
-	env.render()
-	observation, reward, done, info = env.step(action)
-	action = best_agent.step(observation, reward)
-	total_reward += reward
-
-	if done:
-		break
+agent.run_epoch(show=True)
