fe81c3424492ef07656ccf6a30489245220a03d3
Kirk Roerig <kirk.roerig@modustri.com>
2016-10-25 21:37:27 -0400

    A step backwards

--- openai/agent.py
+++ openai/agent.py
@@ -2,15 +2,13 @@ import numpy as np
 import os
 
 TRAINING_EPOCHS = 200
-GENERATION_SIZE = 50
+GENERATION_SIZE = 25
 RANDOMIZATION = 1
+STAGNATION_THRESHOLD = 5
 
 
-DESC_SHAPE = (8, 4)
-
-
-def rnd_white(base=None, mag=1):
-    matrix = (np.random.random(DESC_SHAPE) * 2 - 1) * mag
+def rnd_white(base, scale=1, std_dev=1):
+    matrix = (np.random.random(base.shape) * 2 - 1) * scale
 
     if base is not None:
         matrix = matrix + base
@@ -18,8 +16,8 @@ def rnd_white(base=None, mag=1):
     return matrix
 
 
-def rnd_gaussian(base=None, mag=1, std_dev=0.25):
-    matrix = np.random.normal(0, std_dev, DESC_SHAPE) * mag
+def rnd_gaussian(base, scale=1, std_dev=1):
+    matrix = np.random.normal(0, std_dev, base.shape) * scale
 
     if base is not None:
         matrix = matrix + base
@@ -28,13 +26,19 @@ def rnd_gaussian(base=None, mag=1, std_dev=0.25):
 
 
 class Agent:
-    def __init__(self, env, maximize=True):
+    def __init__(self, env, target):
         self.environment = env
-        self.best_candidate = None
-        self.candidates = []
-        self.maximize = maximize
-        self.gen_candidates(std_dev=1)
         self.improvement_rate = 0
+        self.target = target
+        self.learning_rate = 1
+
+        env.reset()
+        observation, reward, done, info = env.step(0)
+        rows = len(observation)
+        columns = env.action_space.n
+
+        self.shape = (rows, columns)
+        self.best_candidate = np.random.random(self.shape)
 
         try:
             os.unlink("learning.csv")
@@ -42,124 +46,126 @@ class Agent:
             pass
 
         try:
-            self.best_candidate = best_candidate = np.loadtxt(fname='params.txt', delimiter=",")
-            self.candidates += [best_candidate]
+            self.best_candidate = np.loadtxt(fname='params.txt', delimiter=",")
             print("Loaded existing params")
         except:
             pass
 
-    def gen_candidates(self, base=None, std_dev=1):
-        self.candidates = []
+    def gen_candidate(self, randomizer=rnd_gaussian, std_dev=1, scale=1):
+        return randomizer(base=self.best_candidate, std_dev=std_dev, scale=scale)
 
-        if isinstance(base, np.ndarray):
-            self.candidates += [base]
-            for _ in range(GENERATION_SIZE):
-                self.candidates += [rnd_gaussian(base=base, mag=RANDOMIZATION, std_dev=std_dev)]
-        elif isinstance(base, list) and isinstance(base[0], np.ndarray):
-            self.candidates += base
-
-            for i in range(GENERATION_SIZE):
-                self.candidates += [rnd_gaussian(base=base[i % len(base)], mag=RANDOMIZATION, std_dev=std_dev)]
-        else:
-            for _ in range(GENERATION_SIZE):
-                self.candidates += [rnd_gaussian(mag=RANDOMIZATION, std_dev=std_dev)]
-
-    def run_epoch(self, desc_matrix=None, show=False):
+    def run_epoch(self, candidate, show=False):
         avg_reward = 0
 
-        if desc_matrix is None:
-            desc_matrix = self.best_candidate
-
         for trial in range(4):
-            trial_reward = 0
-
             self.environment.reset()
             observation, reward, done, info = self.environment.step(0)
+            steps = 200
+            if show:
+                steps += 200
 
-            for _ in range(300):
+            for _ in range(steps):
                 observation = np.array(observation)
-                action_probs = observation.dot(desc_matrix)
+                action_probs = observation.dot(candidate)
 
                 if show:
                     self.environment.render()
+
                 observation, reward, done, info = self.environment.step(np.argmax(action_probs))
-                trial_reward += reward
+                avg_reward += reward
 
                 if done:
                     break
 
-            avg_reward += trial_reward / 4
+        if show:
+            print(avg_reward / 4)
 
-        if show: print(avg_reward)
+        return avg_reward / 4
 
-        return avg_reward
-
-    def evaluate_generation(self):
-        candidate_score = []
+    def evaluate_generation(self, base):
+        t_candidate_score = []
         avg_score = 0
 
         # score the existing candidates
-        for candidate in self.candidates:
-            score = self.run_epoch(desc_matrix=candidate)
-            avg_score += score / float(len(self.candidates))
+        for idx in range(GENERATION_SIZE):
+            candidate = None
 
-            candidate_score += [(candidate, score)]
+            if idx < len(base):
+                candidate = base[idx]
+            else:
+                candidate = rnd_gaussian(base=base[idx % len(base)], std_dev=self.learning_rate, scale=self.learning_rate)
 
-        candidate_score = sorted(candidate_score, key=lambda cs_tuple: cs_tuple[1])
-        best_candidates = [ candidate_score[0] ] # keep the worst
+            score = self.run_epoch(candidate=candidate)
+            avg_score += score / float(GENERATION_SIZE)
 
-        i = len(candidate_score) - 1
-        for _ in range(5):
-            candidate = candidate_score[i]
-            best_candidates += [(candidate[0].copy(), candidate[1])]
-            i -= 1
+            t_candidate_score += [(candidate, score)]
 
-        return best_candidates, avg_score, candidate_score
+        t_candidate_score = sorted(t_candidate_score, key=lambda cs_tuple: cs_tuple[1])
+
+        assert(t_candidate_score[-1][1] > t_candidate_score[0][1])
+
+        return t_candidate_score.pop(), avg_score
 
     def solve(self):
+        first_score = None
         last_gen_avg = -600
-        sliding_std_dev = 1
-        last_best = None
-        last_best_candidates = []
+        last_best_candidates = [[self.best_candidate, last_gen_avg]]
 
         for i in range(TRAINING_EPOCHS):
-            best_candidates, avg_score, all_candidate_scores = self.evaluate_generation()
 
-            self.improvement_rate = avg_score - last_gen_avg
-            sliding_std_dev -= 0.005 * self.improvement_rate
-            sliding_std_dev = max(sliding_std_dev, 0.2)
+            # # re score all the best old candidates
+            # for old_best in last_best_candidates:
+            #     score = self.run_epoch(candidate=old_best[0])
+            #     old_best[1] = (old_best[1] + score) / 2
 
+            # evaluate the generation based off of the old bests
+            (best_candidate, best_score), avg_score = self.evaluate_generation(base=list(map(lambda c: c[0], last_best_candidates)))
+
+            # evaluate how much improvment has taken place between generations
+            iteration_improvement = avg_score - last_gen_avg
+            self.improvement_rate = self.improvement_rate * .75 + iteration_improvement * .25
+
+            # set the 'first score' which dictates with how much variation random candidates are generated
+            # if improvement stagnates, this is reset as if learning just began
+            if first_score is None:
+                first_score = avg_score
+                last_gen_avg = avg_score
+
+            if self.improvement_rate < STAGNATION_THRESHOLD:
+                first_score = first_score * 0.75 + avg_score * 0.25
+
+            self.learning_rate = (1 - max(0.0001, (last_gen_avg - first_score) / (self.target - first_score)))
             last_gen_avg = avg_score
 
-            best_best = best_candidates[len(best_candidates) - 1]
+            print("(%d/%d) Best score: %f Avg: %f Improvement: %f ∆I: %f  ∑: %f" %
+                  (i, TRAINING_EPOCHS, best_score, avg_score, iteration_improvement, self.improvement_rate, self.learning_rate))
 
-            if len(last_best_candidates) < 5:
-                last_best_candidates += [best_best[0].copy()]
-            else:
-                last_best_candidates[i % len(last_best_candidates)] = best_best[0].copy()
-
-            print("(%d/%d) Best score: %f Avg: %f Improvement: %f ∑: %f" % (i, TRAINING_EPOCHS, best_best[1], avg_score, self.improvement_rate, sliding_std_dev))
-
-            if avg_score >= 200 and i > 10:
+            # are we done???
+            if avg_score - first_score >= self.target - first_score:
                 break
+
+            # trim up
+            last_best_candidates += [[best_candidate.copy(), best_score + avg_score]]
+            last_best_candidates = sorted(last_best_candidates, key=lambda cs_tuple: cs_tuple[1])
+            if len(last_best_candidates) > 10:
+                last_best_candidates = last_best_candidates[1:len(last_best_candidates)]
+
+            self.best_candidate = last_best_candidates[-1][0].copy()
+            best_score = last_best_candidates[-1][1]
 
             # in the event of a regression, use the best from the last improvement
             if self.improvement_rate < 0:
                 print("!!!Regression!!!")
-                sliding_std_dev = 1
 
-            best_matrices = [ ] + last_best_candidates
-            for candidate in best_candidates: best_matrices += [candidate[0]]
+            # save current best candidate
+            np.savetxt(fname='params.txt', X=best_candidate, delimiter=',')
 
-            self.gen_candidates(base=best_matrices, std_dev = sliding_std_dev)
-            self.best_candidate = best_matrices.pop()
-
-            np.savetxt(fname='params.txt', X=self.best_candidate, delimiter=',')
-
+            # save learning profile
             with open("learning.csv", mode="a+") as history:
-                matrix_elements = ""
-                reshaped = best_best[0].reshape(best_best[0].size)
-                for element in reshaped:
-                    matrix_elements += str(element) + ','
+                # matrix_elements = ""
+                # reshaped = best_candidate.reshape(best_candidate.size)
+                # for element in reshaped:
+                #     matrix_elements += str(element) + ','
 
-                history.write(matrix_elements + str(best_best[1]) + "\n")
+                history.write(
+                    "{0},{1},{2}\n".format(str(best_score), str(self.improvement_rate), str(self.learning_rate)))
--- openai/lander.py
+++ openai/lander.py
@@ -4,10 +4,10 @@ from getopt import getopt
 import sys
 
 env = gym.make('LunarLander-v2')
-agent = Agent(env)
+agent = Agent(env, target=200)
 
 if not '--just-show' in sys.argv:
 	print("Solving")
 	agent.solve()
 
-agent.run_epoch(show=True)
+agent.run_epoch(show=True, candidate=agent.best_candidate)
