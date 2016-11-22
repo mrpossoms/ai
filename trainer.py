0d8b7d70a415775403293ce0f4b0266e71583e01
Kirk Roerig <mr.possoms@gmail.com>
2016-11-21 19:00:30 -0500

    Refactored a little bit. Training is now done with the Trainer class

--- trainer.py
+++ trainer.py
@@ -2,39 +2,48 @@ import numpy as np
 from threading import Thread
 
 class Subject:
-	def __init__(self, rep, maximizing=True):
+	def __init__(self, rep):
 		self.soln = rep
 		self.g_soln_space = np.zero(rep.shape)
 		self.d_score = np.zero(rep.shape) 
+
+
+class Trainer:
+	def __init__(self, maximizing=True):
+                self.maximizing = maximizing
 		self.gamma = 0.001 # learning rate
-	
 		self.thread_count = 4
 
-	def compute_gradient(self, col_range):
-		cand = self.soln.copy()
-		score_0 = self.avg_evaluation(cand)
+	def compute_gradient(self, candidate, col_range, resource):
+		score_0 = self.avg_evaluation(candidate.soln, resource)
 
 		for x in col_range: 
-			for y in range(cand.shape[1]):
-				dxy = cand.copy()
+			for y in range(candidate.shape[1]):
+				dxy = candidate.soln.copy()
 				dxy[x][y] += self.gamma
-				diff = score_0 - self.avg_evaluation(dxy)  
-				self.d_score = diff
-				self.g_soln_space[x][y] = (self.gamma * self.diff) / self.gamma
+				diff = score_0 - self.avg_evaluation(dxy, resource)  
+				candidate.d_score = diff
+				candidate.g_soln_space[x][y] = (self.gamma * self.diff) / self.gamma
 
-	def train(self, iterations):
-		col_per_thread = self.soln.shape[0]
+        def subprocess_resource(self):
+            raise NotImplemented
+
+	def train(self, iterations, candidate):
+		col_per_thread = candidate.soln.shape[0]
                 threads = []
 
 		for i in range(self.thread_count):
 			thread = Thread(target=self.compute_gradient, kwargs={
                                 "self": self,
-                                "col_range": range(i * col_per_thread, (i + 1) * col_per_thread)
+                                "candidate": candidate,
+                                "col_range": range(i * col_per_thread, (i + 1) * col_per_thread),
+                                "resource": self.subprocess_resource()
                             })
 
                         threads += [thread]
 
                 for i in range(iterations):
+                    # start computing gradients
                     for thread in threads:
                         thread.run()
 
@@ -43,7 +52,7 @@ class Subject:
                        
                     # modify solution matrix values by gradient
                     # computed in the previous run
-                    self.soln += self.g_soln_space
+                    candidate.soln += candidate.g_soln_space
 
 
 	def evaluate(self, candiate):
@@ -56,8 +65,5 @@ class Subject:
 		
 		return score / iterations
 
-class Trainer:
-	def __init__(self):
-		pass
 
 	
