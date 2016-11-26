77336325287802ab51ae5135a2a26244ee4c88dd
Kirk Roerig <mr.possoms@gmail.com>
2016-11-26 15:49:50 -0500

    Fixed runtime errors. Working on correcting behavior

--- trainer.py
+++ trainer.py
@@ -1,71 +1,91 @@
 import numpy as np
 from threading import Thread
+from threading import Lock
 
 class Subject:
-	def __init__(self, rep):
-		self.soln = rep
-		self.g_soln_space = np.zero(rep.shape)
-		self.d_score = np.zero(rep.shape) 
+	def __init__(self, rep, path=None):
+		self.name = path
+		
+		if path is not None:
+			try:
+				self.soln = np.loadtxt(fname=path, delimiter=',')
+			except:
+				self.soln = rep
+                else:
+			self.soln = rep
+
+                if len(self.soln.shape) < 2:
+                    self.soln = self.soln.reshape((self.soln.shape[0], 1))
+
+		self.g_soln_space = np.zeros(self.soln.shape)
+		self.d_score = np.zeros(self.soln.shape) 
 
 
+	def save(self):
+		np.savetxt(fname=name, X=self.soln, delimiter=',')
+
+def compute_gradient(trainer, candidate, col_range, resource):
+    #print("I'm a thread", str(candidate.soln.shape[1]), col_range)
+    for x in col_range: 
+        for y in range(candidate.soln.shape[1]):
+                dxy = candidate.soln.copy()
+                dxy[x][y] += trainer.gamma
+                diff = trainer.diff(candidate.soln, dxy, resource)  
+                candidate.d_score = diff
+                candidate.g_soln_space[x][y] = (trainer.gamma * diff) / trainer.gamma
+
+   
 class Trainer:
 	def __init__(self, maximizing=True):
                 self.maximizing = maximizing
 		self.gamma = 0.001 # learning rate
 		self.thread_count = 4
                 self.os = ObservationSpace(0.25)
-
-	def compute_gradient(self, candidate, col_range, resource):
-		for x in col_range: 
-			for y in range(candidate.shape[1]):
-				dxy = candidate.soln.copy()
-				dxy[x][y] += self.gamma
-				diff = self.diff(candidate.soln, dxy, resource)  
-				candidate.d_score = diff
-				candidate.g_soln_space[x][y] = (self.gamma * self.diff) / self.gamma
+                print('Trainer initialized', self.os)
 
         def subprocess_resource(self, subproc_idx):
             raise NotImplemented
 
 	def train(self, iterations, candidate):
-		col_per_thread = candidate.soln.shape[0]
-                threads = []
-
-                # initialize threads, providing them with the needed resources
-                # and values to perform gradient computations
-		for i in range(self.thread_count):
-			thread = Thread(target=self.compute_gradient, kwargs={
-                                "self": self,
-                                "candidate": candidate,
-                                "col_range": range(i * col_per_thread, (i + 1) * col_per_thread),
-                                "resource": self.subprocess_resource()
-                            })
-
-                        threads += [thread]
+		col_per_thread = candidate.soln.shape[0] / self.thread_count
 
                 for i in range(iterations):
                     # reset the observation-space table
                     self.os.clear()
-                    
-                    # start computing gradients
-                    for thread in threads:
-                        thread.run()
+                    threads = []
+
+                    # initialize threads, providing them with the needed resources
+                    # and values to perform gradient computations
+                    for i in range(self.thread_count):
+                            thread = Thread(target=compute_gradient, kwargs={
+                                    "trainer": self,
+                                    "candidate": candidate,
+                                    "col_range": range(i * col_per_thread, (i + 1) * col_per_thread),
+                                    "resource": self.subprocess_resource(subproc_idx=i)
+                                })
+
+                            threads += [thread]
+                            thread.run()
 
                     # finish computing gradients
                     for thread in threads:
-                        thread.join()
+                        if thread.isAlive():
+                            thread.join()
                        
                     # modify solution matrix values by gradient
                     # computed in the previous run
                     candidate.soln += candidate.g_soln_space
+                    start, score = self.evaluate(candidate.soln, self.subprocess_resource(0))
+                    print("score %f" % score)
 
         def diff(self, old_candidate, new_candidate, resource):
+            #print("observation space:", self.os)
             while True:
                 # generate some trial results with the original candidate
                 # store them in the observation-space table
                 for _ in range(10):
-                    result = self.evaluate(old_candidate, resource) 
-                    self.os += result
+                    start, score = self.evaluate(old_candidate, resource) 
+                    self.os + (start, score)
 
                 # run the altered candidate, collect the starting conditions and score
                 start, score = self.evaluate(new_candidate, resource)
@@ -86,17 +106,17 @@ class ObservationSpace():
     def __init__(self, match_threshold):
         self.observations = []
         self.threshold = match_threshold
-        self.lock = threading.Lock()
+        self.lock = Lock()
 
     def clear(self):
         self.observations = []
 
     def __add__(self, other):
         self.lock.acquire()
-        self.observations += other
+        self.observations.append(other)
         self.lock.release()
 
-    def __getitem_(self, key):
+    def __getitem__(self, key):
         nearest = []
 
         self.lock.acquire()
