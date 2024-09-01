37401a00f2fddd0f2f0b9295e13c92fbb9902911
Kirk Roerig <mr.possoms@gmail.com>
2024-09-01 09:56:54 -0600

    Missed policy.hpp

---
+++ policy-gradient/REINFORCE/torch/policy.hpp
@@ -0,0 +1,79 @@
+#pragma once
+#include <unistd.h>
+#include <tuple>
+#include <vector>
+
+#include "torch/torch.h"
+#include "env.hpp"
+
+
+struct Trajectory
+{
+	struct Frame
+	{
+		torch::Tensor state;
+		torch::Tensor action_probs;
+		// torch::Tensor action;
+		unsigned action_idx;
+		float reward;
+	};
+
+	static float R(const std::vector<Frame>& T, float gamma=0.999f)
+	{
+		float r = 0.0f;
+		for (int i = 0; i < T.size(); i++)
+		{
+			r += pow(gamma, i) * T[i].reward;
+		}
+
+		return r;
+	}
+};
+
+namespace policy
+{
+	struct Policy
+	{
+		// virtual bool load_params(const std::string& path) = 0;
+		// virtual void save_params(const std::string& path) = 0;
+		virtual void act(const std::vector<float>& x, Environment& env, std::vector<Trajectory::Frame>& traj) = 0;
+		virtual void train(const std::vector<Trajectory::Frame>& traj, float learning_rate) = 0;
+	};
+
+	struct Discrete : public Policy, torch::nn::Module
+	{
+		Discrete(size_t observation_size, size_t action_size);
+
+		// bool load_params(const std::string& path) override;
+
+		// void save_params(const std::string& path) override;
+
+		torch::Tensor forward(torch::Tensor x);
+
+		virtual void act(const std::vector<float>& x, Environment& env, std::vector<Trajectory::Frame>& traj) override;
+		virtual void train(const std::vector<Trajectory::Frame>& traj, float learning_rate) override;
+	private:
+		torch::nn::Linear l0 = nullptr, l1 = nullptr, l2 = nullptr, l3 = nullptr;
+	};
+
+
+	struct hyper_parameters
+	{
+		unsigned epochs = 1;
+		unsigned batch_size = 1;
+		float learning_rate = 0.001f;
+	};
+
+	void init(size_t observation_size, size_t action_size);
+
+	bool loaded();
+
+	void save(const std::string& path);
+
+	void train_policy_gradient(const std::vector<Trajectory::Frame>& traj, const hyper_parameters& hp={});
+
+	torch::Tensor act_probs(torch::Tensor x);
+
+	int act(torch::Tensor probs, bool stochastic=true);
+
+}
